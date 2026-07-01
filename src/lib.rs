a356c9542eef3a863d1dd3375bd981af4c2045c4
Harold Favereau <harold.favereau@gmail.com>
2026-07-01 20:57:34 +0200

    ibm

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,13 @@ pub enum FlippedOffResult {
     No,
 }
 
+const SCREEN_WIDTH: u8 = 64;
+const SCREEN_HEIGHT: u8 = 32;
+
 pub trait Display {
-    fn draw(&mut self, x: u8, y: u8, draw: bool) -> FlippedOffResult;
+    fn switch(&mut self, x: u8, y: u8) -> FlippedOffResult;
+
+    fn redraw(&mut self);
 
     fn clear(&mut self);
 }
@@ -96,17 +101,37 @@ impl<D: Display, KR: KeyboardReader> Emulator<D, KR> {
                 self.registers[get_x(opcode) as usize] = get_nn(opcode)
             }
             Instruction::AddRegisterConstant => {
-                let _ = self.registers[get_x(opcode) as usize].overflowing_add(get_nn(opcode));
+                self.registers[get_x(opcode) as usize] = self.registers[get_x(opcode) as usize]
+                    .overflowing_add(get_nn(opcode))
+                    .0;
             }
             Instruction::SetIndexRegister => self.index_register = get_nnn(opcode),
             Instruction::Draw => {
-                let x = self.registers[get_x(opcode) as usize] & 63;
-                let y = self.registers[get_y(opcode) as usize] & 31;
+                let x = self.registers[get_x(opcode) as usize] & (SCREEN_WIDTH - 1);
+                let y = self.registers[get_y(opcode) as usize] & (SCREEN_HEIGHT - 1);
                 self.registers[0xf] = 0;
                 let n = get_n(opcode);
-                for r in 0..n {
-                    let sprite_byte = self.memory[self.index_register as usize];
+                for iy in 0..n {
+                    let sprite_byte = self.memory[(self.index_register + (iy as u16)) as usize];
+
+                    for ix in 0..8 {
+                        let should_switch = sprite_byte & (1 << (7 - ix)) != 0;
+                        if should_switch
+                            && matches!(self.display.switch(x + ix, y + iy), FlippedOffResult::Yes)
+                        {
+                            self.registers[0xf] = 1;
+                        }
+                        if x + ix == SCREEN_WIDTH - 1 {
+                            break;
+                        }
+                    }
+
+                    if y + iy == SCREEN_HEIGHT - 1 {
+                        break;
+                    }
                 }
+
+                self.draw_flag = true;
             }
             _ => todo!(),
         }
@@ -138,7 +163,7 @@ impl<D: Display, KR: KeyboardReader> Emulator<D, KR> {
             }
 
             if self.draw_flag {
-                //render_screen();
+                self.display.redraw();
                 self.draw_flag = false;
             }
 
@@ -166,6 +191,11 @@ impl<D: Display, KR: KeyboardReader> Emulator<D, KR> {
                     self.sound_timer -= 1;
                 }
             }
+
+            if self.draw_flag {
+                self.display.redraw();
+                self.draw_flag = false;
+            }
         }
     }
 }
@@ -190,48 +220,48 @@ fn get_nnn(opcode: u16) -> u16 {
     opcode & 0xFFF
 }
 
+#[derive(Debug)]
 enum Instruction {
-    ClearScreen,
-    Jump,
-    JumpOffset,
-    SetRegisterConstant,
-    AddRegisterConstant,
-    CopyBetweenRegister,
-    SetBetweenRegister,
-    SetOrRegister,
-    SetAndRegister,
-    SetXorRegister,
-    AddBetweenRegister,
-    SubBetweenRegister,
-    OppSubBetweenRegister,
-    ShiftRightBetweenRegister,
-    ShiftLeftBetweenRegister,
-    SetIndexRegister,
-    Draw,
-    Return,
-    Subroutine,
-    SkipEqualConstant,
-    SkipUnequalConstant,
-    SkipEqualRegister,
-    SkipUnequalRegister,
-    MachineSubroutine,
-    RandomNumber,
-    SkipKeyHeld,
-    SkipKeyNotHeld,
-    SetRegisterFromDelayTimer,
-    WaitKeyPress,
-    SetDelayTimerFromRegister,
-    SetSoundTimerFromRegister,
-    AddIndexRegister,
-    SetIndexSpriteData,
-    StoreBCD,
-    StoreAllRegisters,
-    FillAllRegisters,
+    ClearScreen,               //0NNN
+    Jump,                      //1NNN
+    JumpOffset,                //BNNN
+    SetRegisterConstant,       //6XNN
+    AddRegisterConstant,       //7XNN
+    SetBetweenRegister,        //8XY0
+    SetOrRegister,             //8XY1
+    SetAndRegister,            //8XY2
+    SetXorRegister,            //8XY3
+    AddBetweenRegister,        //8XY4
+    SubBetweenRegister,        //8XY5
+    OppSubBetweenRegister,     //8XY7
+    ShiftRightBetweenRegister, //8XY6
+    ShiftLeftBetweenRegister,  //8XYE
+    SetIndexRegister,          //ANNN
+    Draw,                      //DXYN
+    Return,                    //00EE
+    Subroutine,                //2NNN
+    SkipEqualConstant,         //3XNN
+    SkipUnequalConstant,       //4XNN
+    SkipEqualRegister,         //5XY0
+    SkipUnequalRegister,       //9XY0
+    MachineSubroutine,         //0NNN
+    RandomNumber,              //CXNN
+    SkipKeyHeld,               //EX9E
+    SkipKeyNotHeld,            //EXA1
+    SetRegisterFromDelayTimer, //FX07
+    WaitKeyPress,              //FX0A
+    SetDelayTimerFromRegister, //FX15
+    SetSoundTimerFromRegister, //FX18
+    AddIndexRegister,          //FX1E
+    SetIndexSpriteData,        //FX29
+    StoreBCD,                  //FX33
+    StoreAllRegisters,         //FX55
+    FillAllRegisters,          //FX65
 }
 
 impl Instruction {
     fn from_opcode(opcode: u16) -> Self {
-        match opcode << 12 {
+        match opcode >> 12 {
             0x0 => Self::from_opcode_0(opcode),
             0x1 => Self::Jump,
             0x2 => Self::Subroutine,
@@ -248,7 +278,7 @@ impl Instruction {
             0xd => Self::Draw,
             0xe => Self::from_opcode_e(opcode),
             0xf => Self::from_opcode_f(opcode),
-            _ => panic!("unknown instruction"),
+            _ => panic!("unknown instruction {opcode:#x}"),
         }
     }
 
@@ -271,7 +301,7 @@ impl Instruction {
             0x6 => Self::ShiftRightBetweenRegister,
             0x7 => Self::OppSubBetweenRegister,
             0xe => Self::ShiftLeftBetweenRegister,
-            _ => panic!("unknown instruction"),
+            _ => panic!("unknown instruction {opcode:#x}"),
         }
     }
 
@@ -279,7 +309,7 @@ impl Instruction {
         match opcode & 0xff {
             0x9e => Self::SkipKeyHeld,
             0xa1 => Self::SkipKeyNotHeld,
-            _ => panic!("unknown instruction"),
+            _ => panic!("unknown instruction {opcode:#x}"),
         }
     }
 
@@ -294,7 +324,134 @@ impl Instruction {
             0x33 => Self::StoreBCD,
             0x55 => Self::StoreAllRegisters,
             0x65 => Self::FillAllRegisters,
-            _ => panic!("unknown instruction"),
+            _ => panic!("unknown instruction {opcode:#x}"),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::{
+        cell::RefCell,
+        io::{Write, stdout},
+        rc::Rc,
+    };
+
+    use crate::{Display, Emulator, FlippedOffResult, KeyboardReader, SCREEN_HEIGHT, SCREEN_WIDTH};
+
+    struct DummyDisplay {
+        current: [[bool; SCREEN_WIDTH as usize]; SCREEN_HEIGHT as usize],
+        buffer: [[bool; SCREEN_WIDTH as usize]; SCREEN_HEIGHT as usize],
+    }
+
+    fn get_pretty(value: bool) -> &'static [u8] {
+        match value {
+            true => "██".as_bytes(),
+            false => "  ".as_bytes(),
+        }
+    }
+
+    fn get_direct(value: bool) -> &'static [u8] {
+        match value {
+            true => "#".as_bytes(),
+            false => " ".as_bytes(),
+        }
+    }
+
+    impl DummyDisplay {
+        fn new() -> Self {
+            let current = [[false; SCREEN_WIDTH as usize]; SCREEN_HEIGHT as usize];
+            let buffer = [[false; SCREEN_WIDTH as usize]; SCREEN_HEIGHT as usize];
+            DummyDisplay { current, buffer }
+        }
+
+        fn write<W: Write>(&self, mut writer: W, pretty: bool) -> std::io::Result<()> {
+            for row in &self.current {
+                for &pixel in row {
+                    writer.write_all(if pretty {
+                        get_pretty(pixel)
+                    } else {
+                        get_direct(pixel)
+                    })?;
+                }
+
+                writer.write_all(b"\n")?;
+            }
+
+            Ok(())
+        }
+
+        fn write_pretty<W: Write>(&self, mut writer: W) -> std::io::Result<()> {
+            self.write(writer, true)
+        }
+
+        fn write_direct<W: Write>(&self, mut writer: W) -> std::io::Result<()> {
+            self.write(writer, false)
+        }
+    }
+
+    impl Display for DummyDisplay {
+        fn switch(&mut self, x: u8, y: u8) -> crate::FlippedOffResult {
+            self.buffer[y as usize][x as usize] = !self.buffer[y as usize][x as usize];
+            match self.buffer[y as usize][x as usize] {
+                true => FlippedOffResult::No,
+                false => FlippedOffResult::Yes,
+            }
+        }
+
+        fn redraw(&mut self) {
+            self.current.copy_from_slice(&self.buffer);
+        }
+
+        fn clear(&mut self) {
+            self.buffer = [[false; SCREEN_WIDTH as usize]; SCREEN_HEIGHT as usize];
+        }
+    }
+
+    impl Display for Rc<RefCell<DummyDisplay>> {
+        fn switch(&mut self, x: u8, y: u8) -> crate::FlippedOffResult {
+            self.borrow_mut().switch(x, y)
+        }
+
+        fn redraw(&mut self) {
+            self.borrow_mut().redraw()
+        }
+
+        fn clear(&mut self) {
+            self.borrow_mut().clear()
+        }
+    }
+
+    struct DummyKeyboard;
+
+    impl KeyboardReader for DummyKeyboard {
+        fn is_key_pressed(key: u8) -> bool {
+            todo!()
+        }
+
+        fn get_pressed_key() -> u8 {
+            todo!()
+        }
+    }
+
+    #[test]
+    fn load_ibm() {
+        let program = std::fs::read("./programs/ibm-logo.ch8").unwrap();
+
+        let display = Rc::new(RefCell::new(DummyDisplay::new()));
+        let keyboard_reader = DummyKeyboard {};
+        let mut emulator = Emulator::init(Rc::clone(&display), keyboard_reader, &program);
+
+        emulator.run_full_speed(100);
+
+        let expected = std::fs::read("./results/ibm-logo.txt").unwrap();
+        let mut actual = Vec::new();
+        display.borrow().write_direct(&mut actual).unwrap();
+
+        assert_eq!(actual, expected, "\nActual screen:\n{}", {
+            let mut pretty = Vec::new();
+            display.borrow().write_pretty(&mut pretty).unwrap();
+            String::from_utf8(pretty).unwrap()
+        });
     }
 }
